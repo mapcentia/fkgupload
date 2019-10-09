081a825b548e275fac8ba7cc84b2a624fdc3e935
Martin Høgh <mh@mapcentia.com>
2019-10-09 10:09:26 +0200

    straekn_nr added again

--- api/Process.php
+++ api/Process.php
@@ -152,7 +152,7 @@ class Process extends \app\inc\Controller
 
                 "navn" => ["navn", false, "varchar"],
                 "navndels" => ["navndels", false, "varchar"],
-                // "straekn_nr" => ["straekn_nr", false, "varchar"],
+                "straekn_nr" => ["straekn_nr", false, "varchar"],
 
                 "afm_rute_k" => ["afm_rute_k", false, "int"],
                 "laengde" => ["laengde", false, "numeric"],
