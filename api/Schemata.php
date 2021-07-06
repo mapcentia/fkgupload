4df6cb314fe31bffba7dec604047d2e7d095ab1a
Martin Høgh <mh@mapcentia.com>
2021-07-06 17:50:01 +0200

    adr_id and vejkode commented out

--- api/Schemata.php
+++ api/Schemata.php
@@ -120,7 +120,7 @@ class Schemata
             "betaling_k" => ["betaling_k", false, "int"],
             "book_k" => ["book_k", false, "int"],
             "antal_pl" => ["antal_pl", false, "int"],
-            "adr_id" => ["adr_id", false, "uuid"],
+            //"adr_id" => ["adr_id", false, "uuid"],
 
             "foto_link" => ["foto_link", false, "varchar"],
             "foto_link1" => ["foto_link1", false, "varchar"],
@@ -153,9 +153,9 @@ class Schemata
             "geometri" => ["the_geom", true, "geometry"],
 
             "saeson_bem" => ["saeson_bem", false, "varchar"],
-            "vejkode" => ["vejkode", false, "int"],
+            //"vejkode" => ["vejkode", false, "int"],
             "vejnavn" => ["vejnavn", false, "varchar"],
-            "cvf_vejkode" => ["cvf_vejkode", false, "varchar"],
+            //"cvf_vejkode" => ["cvf_vejkode", false, "varchar"],
             "husnr" => ["husnr", false, "varchar"],
             "postnr" => ["postnr", false, "int"],
             "postnr_by" => ["postnr_by", false, "varchar"],
@@ -191,7 +191,7 @@ class Schemata
             "betaling_k" => ["betaling_k", false, "int"],
             "book_k" => ["book_k", false, "int"],
             "antal_pl" => ["antal_pl", false, "int"],
-            "adr_id" => ["adr_id", false, "uuid"],
+            //"adr_id" => ["adr_id", false, "uuid"],
 
             "foto_link" => ["foto_link", false, "varchar"],
             "foto_link1" => ["foto_link1", false, "varchar"],
@@ -224,9 +224,9 @@ class Schemata
             "geometri" => ["the_geom", true, "geometry"],
 
             "saeson_bem" => ["saeson_bem", false, "varchar"],
-            "vejkode" => ["vejkode", false, "int"],
+            //"vejkode" => ["vejkode", false, "int"],
             "vejnavn" => ["vejnavn", false, "varchar"],
-            "cvf_vejkode" => ["cvf_vejkode", false, "varchar"],
+            //"cvf_vejkode" => ["cvf_vejkode", false, "varchar"],
             "husnr" => ["husnr", false, "varchar"],
             "postnr" => ["postnr", false, "int"],
             "postnr_by" => ["postnr_by", false, "varchar"],
@@ -274,7 +274,7 @@ class Schemata
             "certifi_k" => ["certifi_k", false, "int"],
             "hierarki_k" => ["hierarki_k", false, "int"],
             "gpx_link" => ["gpx_link", false, "varchar"],
-            "adr_id" => ["adr_id", false, "uuid"],
+            //"adr_id" => ["adr_id", false, "uuid"],
 
             "foto_link" => ["foto_link", false, "varchar"],
             "foto_link1" => ["foto_link1", false, "varchar"],
@@ -305,9 +305,9 @@ class Schemata
             "geometri" => ["the_geom", true, "geometry"],
 
             "saeson_bem" => ["saeson_bem", false, "varchar"],
-            "vejkode" => ["vejkode", false, "int"],
+            //"vejkode" => ["vejkode", false, "int"],
             "vejnavn" => ["vejnavn", false, "varchar"],
-            "cvf_vejkode" => ["cvf_vejkode", false, "varchar"],
+            //"cvf_vejkode" => ["cvf_vejkode", false, "varchar"],
             "husnr" => ["husnr", false, "varchar"],
             "postnr" => ["postnr", false, "int"],
             "postnr_by" => ["postnr_by", false, "varchar"],
