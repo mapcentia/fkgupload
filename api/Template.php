71acbd7d7fb037114aea8529b221f95201ddd1ed
Martin Høgh <mh@mapcentia.com>
2021-07-07 11:13:20 +0200

    Template init

---
+++ api/Template.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @author     Martin Høgh <mh@mapcentia.com>
+ * @copyright  2013-2021 MapCentia ApS
+ * @license    http://www.gnu.org/licenses/#AGPL  GNU AFFERO GENERAL PUBLIC LICENSE 3
+ *
+ */
+
+namespace app\extensions\fkgupload\api;
+
+use app\inc\Controller;
+use app\inc\Route;
+use app\models\Database;
+use app\models\Sql;
+use app\inc\Session;
+use Phpfastcache\Exceptions\PhpfastcacheInvalidArgumentException;
+
+
+/**
+ * Class Template
+ * @package app\extensions\fkgupload\api
+ */
+class Template extends Controller
+{
+    /**
+     * Template constructor.
+     */
+    function __construct()
+    {
+        parent::__construct();
+        Session::start();
+        Session::authenticate(null);
+        Database::setDb(Session::getDatabase());
+    }
+
+    /**
+     * @return array<mixed>|null
+     * @throws PhpfastcacheInvalidArgumentException
+     */
+    public function get_index(): ?array
+    {
+        $format = urldecode(Route::getParam("format"));
+        $layer = Route::getParam("layer");
+        $layerNum = (int)filter_var($layer, FILTER_SANITIZE_NUMBER_INT);
+
+        $fieldArr = [];
+        $schema = Schemata::$schemata[$layerNum];
+        foreach ($schema as $key => $value) {
+            if ($key == "objekt_id") {
+                $fieldArr[] = "NULL AS {$key}";
+            } elseif ($format == "ESRI Shapefile") {
+                $fieldArr[] = "{$key} AS {$value[0]}";
+            } else {
+                $fieldArr[] = $key;
+            }
+        }
+        $fieldStr = implode(",", $fieldArr);
+        $q = "select {$fieldStr} from fkg.{$layer} limit 10";
+        $sql = new Sql("25832");
+        $res = $sql->sql($q, "UTF8", "ogr/" . $format);
+        if (!$res["success"]) {
+            return $res;
+        }
+        return null;
+    }
+}
--- routes/index.php
+++ routes/index.php
@@ -3,3 +3,4 @@
 use \app\inc\Route;
 
 Route::add("extensions/fkgupload/api/process/[action]/[objekt_id]");
+Route::add("extensions/fkgupload/api/template/[layer]/[format]");
